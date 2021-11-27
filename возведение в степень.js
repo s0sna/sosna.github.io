1b8180f17f711c367884ef5aa549d808cd24979e
s0sna <95126093+s0sna@users.noreply.github.com>
2021-11-27 20:48:44 +0500

    ghj

--- возведение в степень.js
+++ возведение в степень.js
@@ -1,9 +1,11 @@
 var sheclo = parseInt(prompt("ведите число"));
 var stepen = parseInt(prompt("ведите степень"));
 var a = 0
-if (a!=stepen){
-    sheclo = sheclo*sheclo;
-    a=a+1
+while (a==0){
+    if (a!=stepen){
+        sheclo = sheclo*sheclo;
+        a=a+1
+    }
 }
 alert (sheclo)
 
