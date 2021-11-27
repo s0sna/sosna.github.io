c171c7c2016965a41e446a67539ae6c1391d31f9
s0sna <95126093+s0sna@users.noreply.github.com>
2021-11-27 20:38:01 +0500

    vozvidenie.html

---
+++ возведение в степень.js
@@ -0,0 +1,12 @@
+var sheclo = parseInt(prompt("ведите число"));
+var stepen = parseInt(prompt("ведите степень"));
+var a = 0
+if (a!=stepen){
+    sheclo = sheclo*sheclo;
+    a=a+1
+}
+alert (sheclo)
+
+
+
+
